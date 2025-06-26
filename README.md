ad17a4adf36e3a05aa933787d37b6c55c07a0462
Torea <31341650+Torea-L@users.noreply.github.com>
2025-06-26 23:09:02 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# D3U
-Code for *Diffusion-based Decoupled Deterministic and Uncertain Framework for Probabilistic Multivariate Time Series Forecasting*
+# Diffusion-based-Decoupled-Deterministic-and-Uncertain-Framework-for-Probabilistic-MTS-Forecasting
+The repo is the official implementation for the paper: Diffusion-based-Decoupled-Deterministic-and-Uncertain-Framework-for-Probabilistic-MTS-Forecasting. 
+## Overall Architecture
+![Logo](./figure/D3U.png)
+## Usage
+Train and evaluate the model. We provide all the above tasks under the folder ./scripts/. You can reproduce the results as the following examples:
+<div style="position: relative;">
+  <pre>
+    <code id="codeBlock">
+# D3U Multivariate Probabilistic forecasting where SparseVQ functions as the conditioning network and PatchDN serves as the denoising
+network.
+bash ./scripts/SVQ/exp_study/
+    </code>
+  </pre>
+  <button style="position: absolute; top: 0; right: 0; padding: 5px; background: #f5f5f5; border: none; cursor: pointer;" onclick="copyCode()">
+    📋
+  </button>
+</div>
+
+## Contact
+If you have any questions or want to use the code, feel free to contact:
+
+- ​**Qi Li**: [li.q@bupt.edu.cn](mailto:li.q@bupt.edu.cn)
+- ​**Zhenyu Zhang**: [zhangzhenyucad@bupt.edu.cn](mailto:zhangzhenyucad@bupt.edu.cn)
+
+
+## Citation
+If you find this repo helpful, please cite our paper.
+
+```markdown
+@inproceedings{lidiffusion,
+  title={Diffusion-based Decoupled Deterministic and Uncertain Framework for Probabilistic Multivariate Time Series Forecasting},
+  author={Li, Qi and Zhang, Zhenyu and Yao, Lei and Li, Zhaoxia and Zhong, Tianyi and Zhang, Yong},
+  booktitle={The Thirteenth International Conference on Learning Representations}
+}
+
+Place the checkpoint of the already trained point prediction model into this folder.
+path: pretrain_checkpoints/SVQ/all/weather/192/checkpoint.pth
---
+++ main.py
@@ -0,0 +1,102 @@
+import random
+
+import numpy as np
+import setproctitle
+import torch
+from model9_NS_transformer.exp.exp_main import Exp_Main
+from utils import params_init
+
+if __name__ == '__main__':
+    setproctitle.setproctitle('D3U_thread')
+
+    args = params_init.get_args()
+
+    if args.seed == -1:
+        fix_seed = np.random.randint(2147483647)
+    else:
+        fix_seed = args.seed
+
+    print('Using seed:', fix_seed)
+
+    random.seed(fix_seed)
+    torch.manual_seed(fix_seed)
+    np.random.seed(fix_seed)
+
+    args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
+    if args.use_gpu:
+        if args.use_multi_gpu:
+            args.devices = args.devices.replace(' ', '')
+            device_ids = args.devices.split(',')
+            args.device_ids = [int(id_) for id_ in device_ids]
+            args.gpu = args.device_ids[0]
+        else:
+            torch.cuda.set_device(args.gpu)
+    print(f'Args in experiment :\n {args}')
+
+    Exp = Exp_Main
+    if args.is_training:
+        for ii in range(args.itr):
+            # setting record of experiments
+            setting = '{}_ts{}_{}_{}_{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_fc{}_eb{}_dt{}_{}_{}'.format(
+                args.decomposition,
+                args.timesteps,
+                args.denoise_model,
+                args.model_id,
+                args.model,
+                args.data_name,
+                args.features,
+                args.seq_len,
+                args.label_len,
+                args.pred_len,
+                args.d_model_c,
+                args.n_heads_c,
+                args.e_layers_c,
+                args.d_layers_c,
+                args.d_ff,
+                args.factor_c,
+                args.embed,
+                args.distil,
+                args.des, ii)
+
+            exp = Exp(args)  # set experiments
+            print('>>>>>>>start training : {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
+            exp.train(setting)
+
+            print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+            # exp.test_cond(setting)
+            exp.test(setting)
+
+            if args.do_predict:
+                print('>>>>>>>predicting : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+                exp.predict(setting, True)
+
+            torch.cuda.empty_cache()
+    else:
+        ii = 0
+        setting = '{}_ts{}_{}_{}_{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_fc{}_eb{}_dt{}_{}_{}'.format(
+            args.decomposition,
+            args.timesteps,
+            args.denoise_model,
+            args.model_id,
+            args.model,
+            args.data_name,
+            args.features,
+            args.seq_len,
+            args.label_len,
+            args.pred_len,
+            args.d_model_c,
+            args.n_heads_c,
+            args.e_layers_c,
+            args.d_layers_c,
+            args.d_ff,
+            args.factor_c,
+            args.embed,
+            args.distil,
+            args.des, ii 
+        )
+        exp = Exp(args)  # set experiments
+        print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+        exp.test(setting, test=1)
+        torch.cuda.empty_cache()
+
+
